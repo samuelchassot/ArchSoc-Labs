e68758e3656c4313994113f428e4d150319961c9
Samuel Chassot <samuel.chassot@epfl.ch>
2018-02-23 09:18:34 +0100

    first parts : start, stop,reset, period changes, irq

--- vhdl/timer.vhd
+++ vhdl/timer.vhd
@@ -19,5 +19,75 @@ entity timer is
 end timer;
 
 architecture synth of timer is
+
+    signal status : std_logic_vector(31 downto 0) := (others => '0');
+    signal control : std_logic_vector(31 downto 0) := (others => '0');
+    signal period : std_logic_vector(31 downto 0) := (others => '0');
+    signal counter : std_logic_vector(31 downto 0) := (others => '0');
+    signal s_TO : std_logic;
+    signal s_ITO : std_logic;
+    signal s_cont : std_logic;
+    signal s_start : std_logic;
+    signal s_stop : std_logic;
+    signal s_run : std_logic;
+
+
 begin
+
+    s_TO <= status(0);
+    s_ITO <= control(0);
+    s_cont <= control(1);
+    s_start <= control(2);
+    s_stop <= control(3);
+    s_run <= status(1);
+    
+    
+    irq <= s_TO and s_ITO;
+
+    reset : process( reset_n )
+    begin
+        if reset_n = '0' then
+            status <= (others => '0');
+            control <= (others => '0');
+            period <= (others => '0');
+            counter <= (others => '0');
+            end if ;
+
+    end process ; -- reset
+
+
+    timeout : process( counter )
+    begin
+        if counter = (others => '0') then
+            status(0) <= '1';
+            counter <= period;
+        end if ;
+    end process ; -- timeout
+
+
+    start : process( s_start )
+    begin
+        if(rising_edge(s_start)) then
+            status(1) <= '1';               -- setting run to 1
+            control(2) <= '0';
+        end if;
+    end process ; -- start
+    
+    stop : process( s_stop )
+    begin
+        if(rising_edge(s_stop)) then
+            status(1) <= '0';               -- setting run to 0
+            control(3) <= '0';
+        end if;
+    end process ; -- stop
+
+    period_changes : process( period )
+    begin
+        control(3) <= '1';
+    end process ; -- period_changes
+
+
+   
+    
+
 end synth;
