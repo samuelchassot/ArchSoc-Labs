e89e3540470dfd58983602daa52925ff7ae33a2c
Unknown <samuel.chassot@epfl.ch>
2018-03-09 10:37:56 +0100

    begin to have a beautiful affichage

--- Lab02/ihandler.asm
+++ Lab02/ihandler.asm
@@ -3,7 +3,6 @@
 .equ	BUTTONS, 	0x2030
 .equ	RAM,		0x1000
 
-
 start: 
 	br main
 
@@ -46,9 +45,9 @@ continue:
 
 ; increments second counter, resets the timer (set TO bit to 0 to ACK the IRQ and reset the timer)
 timer_irs:
-	ldw 	t0, LEDS+4(zero)		; load the second count in t0
+	ldw 	t0, RAM+4(zero)		; load the second count in t0
 	addi	t0, t0, 1			; increment the second timer
-	stw		t0, LEDS+4(zero)		; write back the second counter in memory
+	stw		t0, RAM+4(zero)		; write back the second counter in memory
 	stw		zero, TIMER(zero)	; write 0 in the status register (add = 0) in the TIMER to ACK the IRQ and reset it 
 	ret	
 	
@@ -56,7 +55,7 @@ timer_irs:
 ;increments/decrements third counter, set edgecapture to 0
 button_irs:
 	ldw		t3, BUTTONS+4(zero)	; t3 = edgecapture register
-	ldw		t0, LEDS+8(zero)		; loads third counter in t0
+	ldw		t0, RAM+8(zero)		; loads third counter in t0
 	andi	t2, t3, 1			; isolate in t2 the edgecapture for button 0
 	addi	t1, zero, 1
 	bne 	t2, t1, decr_test
@@ -85,7 +84,7 @@ toggle_test:
 
 end_b_irs:
 	stw		zero, BUTTONS+4(zero) ; resets edgecapture
-	stw		t0, LEDS+8(zero)		;store the third counter
+	stw		t0, RAM+8(zero)		;store the third counter
 	ret
 
 
@@ -109,8 +108,8 @@ main:
 	stw 	t0, TIMER+4(zero)		;start the timer and set continue
 
 	stw		zero, LEDS(zero)			;initialize counters
-	stw		zero, LEDS+4(zero)
-	stw		zero, LEDS+8(zero)
+	stw		zero, RAM+4(zero)
+	stw		zero, RAM+8(zero)
 
 loop:
 	ldw		t0, LEDS(zero)
@@ -118,6 +117,25 @@ loop:
 	stw		t0, LEDS(zero)
 	br loop
 	;implements 3 counters
+
+font_data: 
+	.word 0x7E427E00 ; 0
+ 	.word 0x407E4400 ; 1 
+ 	.word 0x4E4A7A00 ; 2 
+ 	.word 0x7E4A4200 ; 3 
+ 	.word 0x7E080E00 ; 4 
+ 	.word 0x7A4A4E00 ; 5 
+ 	.word 0x7A4A7E00 ; 6 
+ 	.word 0x7E020600 ; 7 
+ 	.word 0x7E4A7E00 ; 8 
+ 	.word 0x7E4A4E00 ; 9 
+ 	.word 0x7E127E00 ; A 
+ 	.word 0x344A7E00 ; B 
+ 	.word 0x42423C00 ; C 
+ 	.word 0x3C427E00 ; D 
+ 	.word 0x424A7E00 ; E 
+ 	.word 0x020A7E00 ; F 
+ 	.word 0x00181800 ; separator
 
 
 
