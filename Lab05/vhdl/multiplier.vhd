228a18eae8dfdb0edb3a8e446e82a6a7ef44e45b
Samuel Chassot <samuel.chassot@epfl.ch>
2018-04-20 10:15:25 +0200

    multiplier 8 finished working

--- Lab05/vhdl/multiplier.vhd
+++ Lab05/vhdl/multiplier.vhd
@@ -14,7 +14,36 @@ entity multiplier is
 end multiplier;
 
 architecture combinatorial of multiplier is
+    signal pp0, pp1, pp2, pp3, pp4, pp5 ,pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13 : unsigned(15 downto 0);
+    signal sB : unsigned(15 downto 0);
+    constant zero : unsigned(15 downto 0) := (others => '0');
 begin
+    sB <= "00000000" & B;
+    -- first level
+    pp0 <= sB when A(0) = '1' else zero;
+    pp1 <= shift_left(sB, 1) when A(1) = '1' else zero; 
+
+    pp2 <= sB when A(2) = '1' else zero;  
+    pp3 <= shift_left(sB, 1) when A(3) = '1' else zero; 
+
+    pp4 <= sB when A(4) = '1' else zero; 
+    pp5 <= shift_left(sB, 1) when A(5) = '1' else zero; 
+
+    pp6 <= sB when A(6) = '1' else zero; 
+    pp7 <= shift_left(sB, 1) when A(7) = '1' else zero; 
+
+    --second level
+    pp8 <= pp0 + pp1;
+    pp9 <= shift_left(pp2 + pp3, 2);
+    pp10 <= pp4 + pp5;
+    pp11 <= shift_left(pp6 + pp7, 2);
+
+    --third level
+    pp12 <= pp8 + pp9;
+    pp13 <= shift_left(pp10 + pp11, 4);
+    
+    P <= pp12 + pp13;
+    
 end combinatorial;
 
 -- =============================================================================
