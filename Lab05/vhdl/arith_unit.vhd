2e694997b81fe2b015a85e14b79870ed85e1dece
Unknown <samuel.chassot@epfl.ch>
2018-04-20 10:54:22 +0200

    Auto stash before merge of "master" and "origin/master"

--- Lab05/vhdl/arith_unit.vhd
+++ Lab05/vhdl/arith_unit.vhd
@@ -33,7 +33,61 @@ architecture combinatorial of arith_unit is
         );
     end component;
 
+    signal sl1, sr1, sr2 : unsigned(7 downto 0);
+    signal sl2, sl3, sl4, sl5, sl6, sr3, sr4, s5, sA : unsigned(15 downto 0);
+    signal sl7, sr6 : unsigned(31 downto 0);
+    constant five : unsigned(7 downto 0) := "00000101";
+    
+
 begin
+    -- left branch of the tree
+    sl1 <= B when sel = '0' else C;
+    
+    sl3 <= "00000000" & C;
+
+    m1 : multiplier port map (
+        A => A;
+        B => sl1;
+        P => sl2
+    );
+
+    sl4 <= sl3 + sl2;
+
+    sl5 <= sl4 when sel = '0' else sl2;
+    sl6 <= sr5 when sel = '0' else sl2;
+
+    m3 : multiplier16 port map(
+        A => sl5;
+        B => sl6;
+        P => sl7
+    );
+
+    -- right branch of the tree
+
+    sr1 <= five when sel = '0' else B;
+    sr2 <= A when sel = '0' else B;
+
+    sA <= "00000000" & A;
+    sr3 <= B when sel = '0' else shift_left(sA, 1);
+
+    m2 : multiplier port map(
+        A => sr1;
+        B => sr2;
+        P => sr4
+    );
+    
+    sr5 <= sr4 + sr3;
+
+    sr6 <= sr5 + sl7;
+
+    -- final multiplexer
+
+    D <= sl7 when sel = '0' else sr6;
+
+    done <= start;
+
+
+
 end combinatorial;
 
 -- =============================================================================
@@ -56,6 +110,7 @@ architecture one_stage_pipeline of arith_unit is
     end component;
 
 begin
+
 end one_stage_pipeline;
 
 -- =============================================================================
