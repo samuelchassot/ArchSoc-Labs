67a06f0e7c72927605fc79f7b3dcccbc844f2370
Unknown <samuel.chassot@epfl.ch>
2018-04-20 11:08:04 +0200

    one-stage pipelining

--- Lab05/vhdl/arith_unit.vhd
+++ Lab05/vhdl/arith_unit.vhd
@@ -110,6 +110,78 @@ architecture one_stage_pipeline of arith_unit is
     end component;
 
 begin
+    signal sl1, sr1, sr2 : unsigned(7 downto 0);
+    signal sl2, sl3, sl4, sl51, sl52, sl61, sl62, sr3, sr4, sr51, sr52 sA : unsigned(15 downto 0);
+    signal sl7, sr6 : unsigned(31 downto 0);
+    signal start2 : std_logic; 
+    constant five : unsigned(7 downto 0) := "00000101";
+    
+
+begin
+    -- left branch of the tree
+    sl1 <= B when sel = '0' else C;
+    
+    sl3 <= "00000000" & C;
+
+    m1 : multiplier port map (
+        A => A;
+        B => sl1;
+        P => sl2
+    );
+
+    sl4 <= sl3 + sl2;
+
+    sl51 <= sl4 when sel = '0' else sl2;
+    sl61 <= sr5 when sel = '0' else sl2;
+
+    m3 : multiplier16 port map(
+        A => sl52;
+        B => sl62;
+        P => sl7
+    );
+
+    register : process( clk, reset_n )
+    begin
+        if(reset_n = '0') then
+            sl52 <= "0000000000000000";
+            sl62 <= "0000000000000000";
+            sr52 <= "0000000000000000";
+            start2 <= '0';
+
+        else if( rising_edge(clk)) then
+            sl52 <= sl51;
+            sl62 <= sl61;
+            sr52 <= sr51;
+            start2 <= start;
+            end if;
+        end if;
+            
+        
+    end process ; -- register
+
+    -- right branch of the tree
+
+    sr1 <= five when sel = '0' else B;
+    sr2 <= A when sel = '0' else B;
+
+    sA <= "00000000" & A;
+    sr3 <= B when sel = '0' else shift_left(sA, 1);
+
+    m2 : multiplier port map(
+        A => sr1;
+        B => sr2;
+        P => sr4
+    );
+    
+    sr51 <= sr4 + sr3;
+
+    sr6 <= sr52 + sl7;
+
+    -- final multiplexer
+
+    D <= sl7 when sel = '0' else sr6;
+
+    done <= start2;
 
 end one_stage_pipeline;
 
