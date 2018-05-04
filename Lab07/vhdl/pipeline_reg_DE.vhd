2250b58b9053657e2ce653f4453fb1f9fb8a2664
Unknown <samuel.chassot@epfl.ch>
2018-05-04 09:32:00 +0200

    fsa

--- Lab07/vhdl/pipeline_reg_DE.vhd
+++ Lab07/vhdl/pipeline_reg_DE.vhd
@@ -37,8 +37,47 @@ entity pipeline_reg_DE is
 end pipeline_reg_DE;
 
 architecture synth of pipeline_reg_DE is
-   
+   constant zero_32 : std_logic_vector(31 downto 0) := others <= '0';
+   constant zero_16 : std_logic_vector(15 downto 0) := others <= '0';
+   constant zero_6 : std_logic_vector(5 downto 0) := others <= '0';
+   constant zero_5 : std_logic_vector(31 downto 0) := others <= '0';
 begin
 
+    reg : process( clk, reset_n )
+    begin
+        if(reset_n = '0') then
+            a_out <= zero_32;
+            b_out <= zero_32;
+            d_imm_out <= zero_32;
+            sel_b_out <= '0';
+            op_alu_out <= zero_6;
+            read_out <= '0';
+            write_out <= '0';
+            sel_pc_out <= '0';
+            branch_op_out <= '0';
+            sel_mem_out <= '0';
+            rf_wren_out <= '0';
+            mux_out <= zero_5;
+            next_addr_out <= zero_16;
+        
+        
+        elsif(rising_edge(clk)) then
+            a_out <= a_in;
+            b_out <= b_in;
+            d_imm_out <= d_imm_in;
+            sel_b_out <= sel_b_in;
+            op_alu_out <= op_alu_in;
+            read_out <= read_in;
+            write_out <= write_in;
+            sel_pc_out <= sel_pc_in;
+            branch_op_out <= branch_op_in;
+            sel_mem_out <= sel_mem_in;
+            rf_wren_out <= rf_wren_in;
+            mux_out <= mux_in;
+            next_addr_out <= next_addr_in;
+
+        end if;
+        
+    end process ; -- reg
 
 end synth;
